baae9a978886acd9a3d032629a453427b792acc4
Krish Maha <krish@mahas.net>
2023-06-01 14:39:33 +0100

    Set up pipeline

---
+++ frontend/test/dummy_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+// import 'package:frontend/test';
+
+
+void main() {
+  testWidgets('Dummy test', (WidgetTester tester) async {
+    String s = "hi";
+    expect(s, "hi");
+  });
+}
