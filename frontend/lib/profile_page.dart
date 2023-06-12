a094d4cc767889be112c392e3740f1b932b96094
shrutip2003 <pradhan2003shruti@gmail.com>
2023-06-12 16:00:21 +0100

    Adding a separate field widget

--- frontend/lib/profile_page.dart
+++ frontend/lib/profile_page.dart
@@ -11,21 +11,25 @@ class _ProfileWidgetState extends State<ProfileWidget> {
   bool isEditing = true;
   String name = "";
   String place = "";
+  String carers = "";
 
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _placeController = TextEditingController();
+  final TextEditingController _carersController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     _nameController.text = name;
     _placeController.text = place;
+    _carersController.text = carers;
   }
 
   @override
   void dispose() {
     _nameController.dispose();
     _placeController.dispose();
+    _carersController.dispose();
     super.dispose();
   }
 
@@ -35,6 +39,7 @@ class _ProfileWidgetState extends State<ProfileWidget> {
       if (isEditing) {
         _nameController.text = name;
         _placeController.text = place;
+        _carersController.text = carers;
       }
     });
   }
@@ -43,6 +48,7 @@ class _ProfileWidgetState extends State<ProfileWidget> {
     setState(() {
       name = _nameController.text;
       place = _placeController.text;
+      carers = _carersController.text;
       isEditing = false;
     });
   }
@@ -95,6 +101,29 @@ class _ProfileWidgetState extends State<ProfileWidget> {
                 place,
                 style: const TextStyle(fontSize: 16.0),
               ),
+        const SizedBox(height: 8.0),
+
+        // Carers --------------------------------------------------------------
+        const Text(
+          'The carers/people who know me the best',
+          style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 53, 113, 217)),
+        ),
+        const SizedBox(height: 8.0),
+        isEditing
+            ? TextField(
+                controller: _carersController,
+                decoration: const InputDecoration(
+                  hintText: 'Enter where you live',
+                ),
+              )
+            : Text(
+                carers,
+                style: const TextStyle(fontSize: 16.0),
+              ),
+        const SizedBox(height: 8.0),
 
         // Edit button ---------------------------------------------------------
         const SizedBox(height: 16.0),
@@ -114,5 +143,46 @@ class _ProfileWidgetState extends State<ProfileWidget> {
         ),
       ],
     ));
+  }
+}
+
+class Field extends StatelessWidget {
+  final String labelText;
+  final String text;
+  final bool isEditing;
+  final TextEditingController controller;
+  const Field(
+      {required this.labelText,
+      required this.text,
+      required this.isEditing,
+      required this.controller,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          labelText,
+          style: const TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 53, 113, 217)),
+        ),
+        const SizedBox(height: 8.0),
+        isEditing
+            ? TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  hintText: 'Enter $labelText',
+                ),
+              )
+            : Text(
+                text,
+                style: const TextStyle(fontSize: 16.0),
+              ),
+        const SizedBox(height: 8.0),
+      ],
+    );
   }
 }
